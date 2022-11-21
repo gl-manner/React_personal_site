326d8545ee261ff3241ea3aca9e496bc14d7c153
Corey Parkins <1993coreyparkins@gmail.com>
2022-11-21 06:24:15 -0800

    Updates

---
+++ precache-manifest.c5af1d334d924b38b173a7ab1d33e2ef.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df6d6392c47b2e0c17a5ba6d09ce2ebd",
+    "url": "/React_personal_site/index.html"
+  },
+  {
+    "revision": "02bb298176b2a3ccc609",
+    "url": "/React_personal_site/static/css/main.f07c1ebe.chunk.css"
+  },
+  {
+    "revision": "2901ae2b4a993529cb9c",
+    "url": "/React_personal_site/static/js/2.4d462590.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/React_personal_site/static/js/2.4d462590.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "02bb298176b2a3ccc609",
+    "url": "/React_personal_site/static/js/main.9135faf7.chunk.js"
+  },
+  {
+    "revision": "3eb7d4cae7109ad7a9b3",
+    "url": "/React_personal_site/static/js/runtime-main.dfb791e7.js"
+  },
+  {
+    "revision": "0c7d949024d988ba6937ea5d1517b1aa",
+    "url": "/React_personal_site/static/media/bg_video.0c7d9490.mp4"
+  },
+  {
+    "revision": "70244f0ab3bf30c698834d326e0c40f5",
+    "url": "/React_personal_site/static/media/photo.70244f0a.jpg"
+  }
+]);
